1f521ad8af650a17a3acb32cbad5947beebce022
Misha Kessler <misha.kessler@gmail.com>
2020-06-09 22:10:52 -0400

    Update landing page.

--- code/src/pages/index.js
+++ code/src/pages/index.js
@@ -7,29 +7,28 @@ import Button from "../components/button"
 
 class IndexPage extends React.Component {
   render() {
-    const siteTitle = "Gatsby Starter Personal Website"
+    const siteTitle = "Six Feet Over"
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <SEO
           title="Home"
-          keywords={[`blog`, `gatsby`, `javascript`, `react`]}
+          keywords={[
+            `home`,
+            `six feet over`,
+            `nonprofit`,
+            `suicide prevention`,
+          ]}
         />
-        <img style={{ margin: 0 }} src="./GatsbyScene.svg" alt="Gatsby Scene" />
         <h1>
           Hey people{" "}
           <span role="img" aria-label="wave emoji">
             👋
           </span>
         </h1>
-        <p>Welcome to your new Gatsby website. You are on your home page.</p>
-        <p>
-          This starter comes out of the box with styled components and Gatsby's
-          default starter blog running on Netlify CMS.
-        </p>
-        <p>Now go build something great!</p>
+        <p>This website is currently being built for Six Feet Over.</p>
         <Link to="/blog/">
-          <Button marginTop="35px">Go to Blog</Button>
+          <Button marginTop="35px">Go to Test Blog</Button>
         </Link>
       </Layout>
     )
